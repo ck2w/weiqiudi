8ee8f7acf0e8bbb2c6f9f6d121b2ad3a19740272
ck2w <chken4869@hotmail.com>
2026-07-08 10:45:51 -0400

    content: add july 8 guide

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import guide20260704Data from "../content/published/2026-07-04.json";
 import guide20260705Data from "../content/published/2026-07-05.json";
 import guide20260706Data from "../content/published/2026-07-06.json";
 import guide20260707Data from "../content/published/2026-07-07.json";
+import guide20260708Data from "../content/published/2026-07-08.json";
 import type {
   DailyGuide,
   KnockoutFixtures as KnockoutFixturesData,
@@ -60,6 +61,7 @@ const publishedGuides = [
   guide20260705Data,
   guide20260706Data,
   guide20260707Data,
+  guide20260708Data,
 ] as DailyGuide[];
 
 const guidesByDate = new Map(publishedGuides.map((guide) => [guide.date, guide]));
